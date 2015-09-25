0b09b196fb099bb2e5eeb81baaf9d55c8344dcb4
Tim Claes <tim.claes@me.com>
2015-09-25 11:56:49 +0200

    Added base templates, updated settings and bugfixes in urls.py

--- filter_app/views.py
+++ filter_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+
+from . import models
 
 # Create your views here.
+
+class OverviewPage(ListView):
+
+	model = models.Module
+	context_object_name = 'module'
+	template_name = 'overview.html'
+
+	def get_queryset(self):
+
+		return self.model.objects.all().prefetch_related()
--- filter_project/urls.py
+++ filter_project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^filters/', include('filter_app.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
