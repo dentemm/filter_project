732de5df079275b14b1e94bd22fe7f254eb3bd6b
Tim Claes <tim.claes@me.com>
2015-09-28 08:19:17 +0200

    Filter swap form template complement. Todo: form validation in view and url integration

--- filter_app/urls.py
+++ filter_app/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
 	url(r'^$', views.OverviewPage.as_view(), name='overview'),
 	url(r'history/$', views.HistoryView.as_view(), name='history'),
 	url(r'^tools/$', views.ToolView.as_view(), name='tools'),
+	url(r'^new/$', views.SwapCreateView.as_view(), name='new-swap'),
+
 
 	url(r'^tools/(?P<fab>[\w-]+)/(?P<tool>[\w-]*)$', views.ToolView.as_view(), name='test'),
 
-	url(r'^new/$', views.SwapCreateView.as_view(), name='new-swap'),
-	
+
 ]
