a178f68675128d8f09cba9ebe3cb9266d5eecbf2
Tim Claes <tim.claes@me.com>
2016-06-24 19:23:32 +0200

    another go

--- filter_app/static/js/statusmail.js
+++ filter_app/static/js/statusmail.js
@@ -7,7 +7,7 @@ $("#statusmail").on('click', function(e){
 
 		type: 'GET',
 		url: 'http://statusmail.imec.be/json/module/list.json',
-		dataType: 'jsonp',
+		dataType: 'json',
 		crossDomain: true,
 		success: success_handler,
 
